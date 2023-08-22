cce607b8b734c6828647338f3ca095285c377b78
jesicayeu <119467018+jesicayeu@users.noreply.github.com>
2023-08-22 19:49:46 +0900

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,7 +5,7 @@ ESX	= nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 RegisterCommand('fps', function(source)
     local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
+    local xPlayer = QBCore.Functions.GetPlayer(_source)
     TriggerClientEvent('fps:openfps', _source)
 end)
 
